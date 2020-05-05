c889924623b33d1eee8a4a8d22fbfe460bbebd5b
Jon Evans <Jevans2497@gmail.com>
2020-05-05 09:08:03 -0500

    Created toolbar and set it as main appBar

--- app/src/main/java/com/example/lydi/MainActivity.kt
+++ app/src/main/java/com/example/lydi/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.lydi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
+
+    private val toolbar: Toolbar by lazy { findViewById<Toolbar>(R.id.toolBar) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
     }
 }
