83b866743fcba665eb926f50d3690a36050900f2
Jon Evans <Jevans2497@gmail.com>
2020-05-06 15:46:08 -0500

    About to mess with textview centering

--- app/src/main/java/com/example/lydi/MainActivity.kt
+++ app/src/main/java/com/example/lydi/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.lydi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +15,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.app_bar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.action_add -> createNew()
+            R.id.action_load -> load()
+            else -> {
+                return super.onOptionsItemSelected(item)
+            }
+        }
+        return false
+    }
+
+    fun createNew() {
+        //Do nothing
+    }
+
+    fun load() {
+        //Do nothing
     }
 }
