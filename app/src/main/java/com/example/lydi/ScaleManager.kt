292e83344284c67f7ead8b3294466fed63ce54c3
Jon Evans <Jevans2497@gmail.com>
2020-05-08 14:39:02 -0500

    Added all the modes and it now works with the timer

--- app/src/main/java/com/example/lydi/ScaleManager.kt
+++ app/src/main/java/com/example/lydi/ScaleManager.kt
@@ -2,15 +2,29 @@ package com.example.lydi
 
 class ScaleManager {
 
-    val notes = Notes().notes
+    var notes = mutableListOf("A", "Bb", "B", "C", "C#", "D", "Eb", "E", "F", "F#", "G", "Ab")
+    var scales = mutableListOf("Major", "minor", "Dorian", "Phrygian", "Lydian", "Mixolydian", "Aeolian", "Locrian")
 
+    //MARK: Enharmonics
+    fun addEnharmonics() {
+        notes.addAll(mutableListOf("A#", "Db", "D#", "Gb", "G#"))
+    }
+
+    fun removeEnharmonics() {
+        notes.removeAll(mutableListOf("A#", "Db", "D#", "Gb", "G#"))
+    }
+
+    //MARK: Scales
     fun selectedScales(): MutableList<String> {
-        return major(notes)
+        var selectedScales = mutableListOf<String>()
+        for (scale in scales) {
+            val scaleToAdd = makeScale(scale)
+            selectedScales.addAll(scaleToAdd)
+        }
+        return selectedScales
     }
 
-    fun major(notes: MutableList<String>): MutableList<String> {
-        return notes.map { "$it Major" } as MutableList<String>
+    fun makeScale(name: String): MutableList<String> {
+        return notes.map { "$it $name" } as MutableList<String>
     }
-
-
 }
