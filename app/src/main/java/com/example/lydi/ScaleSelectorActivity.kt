92b4a2316fb5c3058350d7903481bbf8846836c8
Jon Evans <Jevans2497@gmail.com>
2020-05-12 15:17:10 -0500

    Working on adding listview

--- app/src/main/java/com/example/lydi/ScaleSelectorActivity.kt
+++ app/src/main/java/com/example/lydi/ScaleSelectorActivity.kt
@@ -1,9 +1,18 @@
 package com.example.lydi
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_scale_selector.*
 
 class ScaleSelectorActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
+//    https://developer.android.com/guide/topics/ui/layout/recyclerview
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
