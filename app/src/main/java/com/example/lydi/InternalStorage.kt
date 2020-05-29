24e622f4a7c299dbef334023305319d1fcd5768b
Jon Evans <Jevans2497@gmail.com>
2020-05-29 14:36:29 -0500

    I think I've finally figured out how to read and write text files. Don't know why that took so long

--- app/src/main/java/com/example/lydi/InternalStorage.kt
+++ app/src/main/java/com/example/lydi/InternalStorage.kt
@@ -1,59 +1,81 @@
 package com.example.lydi
 
-import android.os.Environment
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.util.Log
 import com.google.gson.Gson
-import java.io.BufferedReader
-import java.io.FileOutputStream
-import java.io.InputStreamReader
+import java.io.*
 import java.lang.Exception
-import java.lang.StringBuilder
 
-class InternalStorage: AppCompatActivity() {
+//class InternalStorage() {
+//
+//    var context: Context? = null
+//    var file: File? = null
+//    val gson = Gson()
+//
+//    constructor(context: Context) : this() {
+//        this.context = context
+//    }
+//}
 
-    var file = "scalesets"
-    val gson = Gson()
 
-    //MARK: Saving data
-    fun writeToMemory(scaleSets: ScaleSets) {
-        val fileOutputStream: FileOutputStream
-        try {
-            fileOutputStream = openFileOutput(file, 0)
-            val scaleSetsAsGson = convertObjectToGSON(scaleSets)
-            fileOutputStream.write(scaleSetsAsGson.toByteArray())
-            fileOutputStream.flush()
-            fileOutputStream.close()
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
+
+//    MARK: Saving data
+//    fun writeToMemory(scaleSets: ScaleSets) {
+//        val fileOutputStream: FileOutputStream
+//        try {
+//            fileOutputStream = openFileOutput("ScaleSets.txt", 0)
+//            FileOutputStream(file).use {
+//                val scaleSetsAsGson = convertObjectToGSON(scaleSets)
+//                it.write(scaleSetsAsGson.toByteArray())
+//            }
+////            val scaleSetsAsGson = convertObjectToGSON(scaleSets)
+////            fileOutputStream.write(scaleSetsAsGson.toByteArray())
+////            fileOutputStream.flush()
+////            fileOutputStream.close()
+//        } catch (e: Exception) {
+//            e.printStackTrace()
+//        }
+
+    fun writeToMemory(context: Context, scaleSets: ScaleSets) {
+        Log.d("GOOPY", context.filesDir.path)
+//        try {
+//            val fileOut = FileWriter("ScaleSets.txt")
+//            fileOut.write("TEST TEXT")
+//            fileOut.close()
+//        } catch (e: Exception) {
+//            e.printStackTrace()
+//        }
     }
 
     fun readFromMemory(): ScaleSets? {
-        val stringBuilder = StringBuilder()
-        try {
-            var fileInputStream = openFileInput(file)
-            var inputStreamReader = InputStreamReader(fileInputStream)
-            val bufferedReader = BufferedReader(inputStreamReader)
-            var text = bufferedReader.readLine()
-            while (text != null) {
-                stringBuilder.append(text)
-                text = bufferedReader.readLine()
-            }
-            inputStreamReader.close()
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-        val jsonString = stringBuilder.toString()
-        return createObjectFromGSON(jsonString)
+//        val stringBuilder = StringBuilder()
+//        try {
+//            var fileInputStream = openFileInput(file)
+//            var inputStreamReader = InputStreamReader(fileInputStream)
+//            val bufferedReader = BufferedReader(inputStreamReader)
+//            var text = bufferedReader.readLine()
+//            while (text != null) {
+//                stringBuilder.append(text)
+//                text = bufferedReader.readLine()
+//            }
+//            inputStreamReader.close()
+//        } catch (e: Exception) {
+//            e.printStackTrace()
+//        }
+//        val jsonString = stringBuilder.toString()
+//        return createObjectFromGSON(jsonString)
+        return null
     }
 
     fun convertObjectToGSON(scaleSets: ScaleSets): String {
+        val gson = Gson()
         return gson.toJson(scaleSets)
     }
 
     fun createObjectFromGSON(json: String): ScaleSets? {
+        val gson = Gson()
         return gson.fromJson(json, ScaleSets::class.java)
     }
-}
+//}
 
 ////https://medium.com/@smjaejin/easily-reading-and-writing-files-to-internal-storage-with-gson-bdba821ca7de
--- app/src/main/java/com/example/lydi/ScaleSelectorActivity.kt
+++ app/src/main/java/com/example/lydi/ScaleSelectorActivity.kt
@@ -23,7 +23,7 @@ class ScaleSelectorActivity : AppCompatActivity(), CheckBoxInterface {
     private lateinit var viewManager: RecyclerView.LayoutManager
     var allScales = ScaleSetManager().allScales
     var checkedScales = mutableListOf<String>()
-    val internalStorage = InternalStorage()
+//    val internalStorage = InternalStorage()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -80,8 +80,8 @@ class ScaleSelectorActivity : AppCompatActivity(), CheckBoxInterface {
     fun saveClicked() {
         val scaleSet = makeScaleSet()
         val updatedScaleSets = appendScaleSetToScaleSets(scaleSet)
-        internalStorage.writeToMemory(updatedScaleSets)
-        Log.d("TEST", "asdfadsf")
+//        internalStorage.writeToMemory(updatedScaleSets)
+        writeToMemory(this.baseContext, updatedScaleSets)
     }
 
     //MARK: Name Edit Text
@@ -101,7 +101,8 @@ class ScaleSelectorActivity : AppCompatActivity(), CheckBoxInterface {
     }
 
     fun appendScaleSetToScaleSets(scaleSet: ScaleSet): ScaleSets {
-        val currentScaleSets = internalStorage.readFromMemory()
+//        val currentScaleSets = internalStorage.readFromMemory()
+        val currentScaleSets = readFromMemory()
         if (currentScaleSets == null) {
             var newSets = ScaleSets()
             newSets.sets.add(scaleSet)
